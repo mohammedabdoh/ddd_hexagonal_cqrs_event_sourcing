29ec42e2f397d56228047b9287b375733190bcf8
Mohammed Abdoh <firefoxegy@gmail.com>
2020-10-26 21:57:14 +0100

    Added documentation

--- src/Domain/Model/Post/DoctrinePostRepository.php
+++ src/Domain/Model/Post/DoctrinePostRepository.php
@@ -4,7 +4,22 @@ namespace App\Domain\Model\Post;
 
 interface DoctrinePostRepository
 {
+    /**
+     * Fetches a Post aggregate by its ID
+     * @param string $postId
+     * @return Post|null
+     */
     public function byId(string $postId): ?Post;
+
+    /**
+     * Saves a Post aggregate to a MYSQL persistence
+     * @param Post $post
+     */
     public function save(Post $post): void;
+
+    /**
+     * Deletes a Post aggregate from the persistence
+     * @param Post $post
+     */
     public function delete(Post $post): void;
 }
--- src/Domain/Model/Post/ElasticSearchPostRepository.php
+++ src/Domain/Model/Post/ElasticSearchPostRepository.php
@@ -6,6 +6,16 @@ namespace App\Domain\Model\Post;
 
 interface ElasticSearchPostRepository
 {
+    /**
+     * Fetches a Post aggregate index from Elasticsearch by its ID
+     * @param string $id
+     * @return Post|null
+     */
     public function byId(string $id): ?Post;
+
+    /**
+     * Fetch all Post aggregates into an iterable array
+     * @return array
+     */
     public function all(): array;
 }
